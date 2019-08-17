c1eceab937982566f4f7a3859f1661e219fdd2b4
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2019-08-16 21:37:59 -0600

    Player can climb ladders and fall from them

--- Assets/_Project/Scripts/Climber.cs
+++ Assets/_Project/Scripts/Climber.cs
@@ -8,12 +8,29 @@ public class Climber : MonoBehaviour
     private const string LadderLayer = "Ladder";
 
     private Collider2D _colliderComponent;
+    private Rigidbody2D _rigidbodyComponent;
 
     private bool _canClimb;
 
     private void Start()
     {
+        _rigidbodyComponent = GetComponent<Rigidbody2D>();
         _colliderComponent = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        HandleClimbing();
+    }
+
+    private void HandleClimbing()
+    {
+        if (_canClimb)
+        {
+            var direction = Input.GetAxisRaw("Vertical");
+            _rigidbodyComponent.gravityScale = 0f;
+            _rigidbodyComponent.velocity = new Vector2(_rigidbodyComponent.velocity.x, direction* climbingSpeed);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -21,6 +38,16 @@ public class Climber : MonoBehaviour
         if (_colliderComponent.IsTouchingLayers(LayerMask.GetMask(LadderLayer)))
         {
             _canClimb = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!_colliderComponent.IsTouchingLayers(LayerMask.GetMask(LadderLayer)))
+        {
+            Debug.Log("Not climbing anymore!");
+            _canClimb = false;
+            _rigidbodyComponent.gravityScale = 1f;
         }
     }
 }
