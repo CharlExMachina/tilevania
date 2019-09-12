70a72d027e87d0f84b2e92a8758973af15aba7af
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2019-09-11 20:12:17 -0600

    Created EnemyMovement script

---
+++ Assets/_Project/Scripts/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 1f;
+    
+    private Rigidbody2D _rigidbodyComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbodyComponent = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        _rigidbodyComponent.MovePosition(transform.position + Time.deltaTime * movementSpeed * Vector3.right);
+    }
+}
