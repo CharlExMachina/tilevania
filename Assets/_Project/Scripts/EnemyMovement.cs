e6c47a67776f8fb224fb103b71d67d94a5b9bb17
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2019-09-12 21:40:45 -0600

    Enemy goes the opposite direction when it hits a wall

--- Assets/_Project/Scripts/EnemyMovement.cs
+++ Assets/_Project/Scripts/EnemyMovement.cs
@@ -1,14 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 1f;
-    
-    private Rigidbody2D _rigidbodyComponent;
+    [SerializeField] private Vector3 initialDirection;
 
+    private Vector3 _direction;
+    private Rigidbody2D _rigidbodyComponent;
+    private Collider2D _colliderComponent;
+    
     // Start is called before the first frame update
     void Start()
     {
+        _colliderComponent = GetComponent<Collider2D>();
+        _direction = initialDirection;
         _rigidbodyComponent = GetComponent<Rigidbody2D>();
     }
 
@@ -20,6 +26,41 @@ public class EnemyMovement : MonoBehaviour
 
     private void Move()
     {
-        _rigidbodyComponent.MovePosition(transform.position + Time.deltaTime * movementSpeed * Vector3.right);
+        CheckForWalls();
+        _rigidbodyComponent.MovePosition(transform.position + Time.deltaTime * movementSpeed * _direction);
+    }
+
+    private void CheckForWalls()
+    {
+        var rayOrigin = Vector2.zero;
+
+        if (_direction.x > 0) // right
+        {
+            rayOrigin = new Vector2(_colliderComponent.bounds.max.x, 0);
+        }
+        else if (_direction.x < 0)
+        {
+            rayOrigin = new Vector2(_colliderComponent.bounds.min.x, 0);
+        }
+        
+        var filter = new ContactFilter2D
+        {
+            layerMask = LayerMask.GetMask("Ground"),
+            useLayerMask = true,
+            useTriggers = true
+        };
+        
+        var hit = Physics2D.Raycast(transform.position, _direction, 1f, LayerMask.GetMask("Ground"));
+        
+        if (hit)
+        {
+            Debug.Log($"There's a wall ahead, changing direction! {hit.collider.gameObject.layer}");
+            ChangeDirection();
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        _direction *= -1;
     }
 }
