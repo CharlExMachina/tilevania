8c98101cec86b51dbf7d481a1233d6706af27086
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2019-09-17 14:24:51 -0600

    Created SceneLoader class

---
+++ Assets/_Project/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
