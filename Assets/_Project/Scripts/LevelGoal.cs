3146ba09682f73398094967008c86b839a7e762a
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2019-09-17 20:25:24 -0600

    Finished portal transition and next level loads on complete

---
+++ Assets/_Project/Scripts/LevelGoal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LevelGoal : MonoBehaviour
+{
+    [SerializeField] private string sceneToLoad;
+    [SerializeField] private float transitionDelay;
+    
+    private SceneLoader _sceneLoaderComponent;
+
+    private void Start()
+    {
+        _sceneLoaderComponent = GetComponent<SceneLoader>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(GoToNextLevel());
+    }
+
+    private IEnumerator GoToNextLevel()
+    {
+        Time.timeScale = 0.4f;
+        yield return new WaitForSecondsRealtime(transitionDelay);
+        Time.timeScale = 1.0f;
+        _sceneLoaderComponent.LoadScene(sceneToLoad);
+    }
+}
