290e0d3d9ced7b2bf6e7036341c40faf115675ed
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2019-09-19 22:33:10 -0600

    Added GameSession class to store a game session's current state

---
+++ Assets/_Project/Scripts/GameSession.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New game session", menuName = "Tools/New game session", order = 0)]
+public class GameSession : ScriptableObject
+{
+    [SerializeField] private int startingPlayerLives = 3;
+
+    private int _playerLives;
+    private bool _sessionStarted;
+
+    public int PlayerLives => _playerLives;
+
+    public void ResetSession()
+    {
+        _sessionStarted = false;
+    }
+
+    public void InitGame()
+    {
+        if (_sessionStarted) return;
+        
+        _sessionStarted = true;
+        _playerLives = startingPlayerLives;
+    }
+    
+    public void LoseLife()
+    {
+        _playerLives--;
+    }
+}
