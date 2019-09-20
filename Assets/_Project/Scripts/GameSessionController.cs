4e1177cb25acbdd536e1877e13aff245ed39247f
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2019-09-19 22:34:23 -0600

    Added GameSessionController class to handle session state dynamically

---
+++ Assets/_Project/Scripts/GameSessionController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSessionController : MonoBehaviour
+{
+    [SerializeField] private GameSession gameSession;
+
+    private SceneLoader _sceneLoaderComponent;
+
+    private void Start()
+    {
+        _sceneLoaderComponent = GetComponent<SceneLoader>();
+        gameSession.InitGame();
+    }
+
+    public void HandlePlayerDeath()
+    {
+        if (gameSession.PlayerLives > 1)
+        {
+            TakePlayerLife();
+        }
+        else
+        {
+            ResetGameSession();
+        }
+    }
+
+    private void TakePlayerLife()
+    {
+        gameSession.LoseLife();
+        Debug.Log($"Player lost a life! Lives remaining: {gameSession.PlayerLives}");
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        _sceneLoaderComponent.LoadScene(currentSceneIndex);
+    }
+
+    private void ResetGameSession()
+    {
+        gameSession.ResetSession();
+        _sceneLoaderComponent.LoadScene(0);
+    }
+}
