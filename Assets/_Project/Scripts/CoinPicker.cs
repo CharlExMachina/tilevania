10ea6fb6f64da66da14b735db153b9356572d448
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2019-09-21 22:59:23 -0600

    Added CoinPicker class for player to pick up coins

---
+++ Assets/_Project/Scripts/CoinPicker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CoinPicker : MonoBehaviour
+{
+    [SerializeField] private AudioClip pickupSound;
+    
+    private AudioSource _audioSourceComponent;
+
+    private void Start()
+    {
+        _audioSourceComponent = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Interactables") && other.gameObject.CompareTag("Coin"))
+        {
+            _audioSourceComponent.PlayOneShot(pickupSound);
+            Destroy(other.gameObject);
+        }
+    }
+}
