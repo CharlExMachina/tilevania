d477f91b093adfa8e8894e1aa47ef9d8be896a50
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2019-09-24 20:31:44 -0600

    Added lives and score text objects to game UI

---
+++ Assets/_Project/Scripts/LivesText.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class LivesText : MonoBehaviour
+{
+    [SerializeField] private GameSession gameSession;
+    
+    private TextMeshProUGUI _textComponent;
+
+    private void Start()
+    {
+        _textComponent = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        _textComponent.text = $"Lives: {gameSession.PlayerLives}";
+    }
+}
