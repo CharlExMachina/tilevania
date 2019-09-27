ecc34eb99ba341d7cedfaa0980126f36fa144fda
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2019-09-26 21:09:31 -0600

    Added rising water script for water hazard in level 3

---
+++ Assets/_Project/Scripts/RisingWater.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RisingWater : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Transform target;
+
+    private void Update()
+    {
+        var newPos = new Vector3(0, speed * Time.deltaTime);
+        transform.position += newPos;
+        target.position += newPos;
+    }
+}
