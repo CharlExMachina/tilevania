f3a3808a124673b1548dabbdd7d2e6ebf27ef269
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2019-09-28 14:16:45 -0600

    Added fire cannon prefab

---
+++ Assets/_Project/Scripts/FireCannon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class FireCannon : MonoBehaviour
+{
+    public enum FireDirection
+    {
+        Left,
+        Right
+    }
+
+    [SerializeField] private FireDirection direction;
+    [SerializeField] private Transform fireball;
+    [SerializeField] private float fireCooldown;
+    [SerializeField] private float startDelay;
+
+    private bool _startedFire;
+    
+    private void Start()
+    {
+        StartCoroutine(Fire());
+    }
+
+    private IEnumerator Fire()
+    {
+        if (!_startedFire)
+        {
+            yield return new WaitForSeconds(startDelay);
+            _startedFire = true;
+        }
+        
+        switch (direction)
+        {
+            case FireDirection.Left:
+            {
+                var fireballInstance = Instantiate(fireball, transform.position, Quaternion.Euler(0, 0, 90));
+                fireballInstance.GetComponent<SpriteRenderer>().flipY = true;
+                fireballInstance.GetComponent<Fireball>().SetDirection(Vector2.left);
+            }
+                break;
+            case FireDirection.Right:
+            {
+                var fireballInstance = Instantiate(fireball, transform.position, Quaternion.Euler(0, 0, 90));
+                fireballInstance.GetComponent<Fireball>().SetDirection(Vector2.right);
+            }
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
+        yield return new WaitForSeconds(fireCooldown);
+        yield return Fire();
+    }
+}
