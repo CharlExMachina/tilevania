b57ef809f4fbc766fae70bb38022cd16e7ebebc5
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2019-09-28 14:17:05 -0600

    Added fireball prefab with animations

---
+++ Assets/_Project/Scripts/Fireball.cs
@@ -0,0 +1,25 @@
+﻿using TreeEditor;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+
+    private Vector2 _movementDirection;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        _movementDirection = direction;
+    }
+
+    private void Move()
+    {
+        transform.Translate(Time.deltaTime * movementSpeed * _movementDirection, Space.World);
+    }
+}
